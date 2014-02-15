be506d872f132470792f37ab0b83e18b95df1b7c
arrogantrobot <arrogantrobot@gmail.com>
2014-02-15 15:35:51 -0600

    remove jar, add .gitignore, add precomputed powers of two

--- ca.scala
+++ ca.scala
@@ -4,6 +4,8 @@ exec scala -savecompiled "$0" "$@"
 import scala.Math.pow
 
 object CA {
+  val twos = Array(1, 2, 4, 8, 16, 32, 64, 128)
+
   def iterate(row: Vector[Int], cell: Int, rule: Int):Vector[Int] = {
     if (cell == 0) 
       Vector[Int](0) ++ iterate(row, 1, rule)
@@ -14,25 +16,26 @@ object CA {
   }
 
   def getCell(row: Vector[Int], cell: Int, rule: Int): Int = {
-    if (((pow(2,(row(cell - 1) + (row(cell) * 2) + (row(cell + 1) * 4))).toInt) & rule) > 0 ) 1
+    if (((twos(row(cell - 1) + (row(cell) * 2) + (row(cell + 1) * 4))) & rule) > 0 ) 1
     else 0
   }
 
   def showRow(row: Vector[Int]):Unit = {
     println(row.foldLeft("")(_ + _.toString))
   }
+
+  def initAutomaton(count: Int):Vector[Int] = {
+    if (count > 0) 
+      Vector(if (count == width/2) 1 else 0) ++ initAutomaton(count - 1)
+    else 
+      Vector(0)
+  }
 }
 
 
 val width = 175
 
-def thing(count: Int):Vector[Int] = {
-  if (count > 0) 
-    Vector(if (count == width/2) 1 else 0) ++ thing(count - 1)
-  else 
-    Vector(0)
-}
-var row = thing(width)
+var row = CA.initAutomaton(width)
 
 for(n <- 1 to 100) {
   row = CA.iterate(row,0,30 )
